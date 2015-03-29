a3019e1dd99991ec1d85a1b3f62491c9cd28746c
Thomas Meehan <thomas.meehan@living-group.com>
2015-03-29 23:19:41 +0100

    most of the blog

--- README.md
+++ README.md
@@ -27,3 +27,7 @@ Run `grunt` for building and `grunt serve` for preview.
 ## Testing
 
 Running `grunt test` will run the unit tests with karma.
+
+## Notes
+
+The loading gifs are coming from here [preloaders](http://preloaders.net/)
--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -1,6 +1,43 @@
 'use strict';
 
 /**
+ * @ngdoc overview
+ * @name rath3rApp
+ * @description
+ * # rath3rApp
+ *
+ * Main module of the application.
+ */
+angular
+    .module('rath3rApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch'
+    ])
+    .config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+
+        // use the HTML5 History API
+        $locationProvider.html5Mode(true);
+    });
+
+'use strict';
+
+/**
  * @ngdoc function
  * @name rath3rApp.controller:MainCtrl
  * @description
@@ -50,3 +87,21 @@ angular.module('rath3rApp')
 
 
     });
+
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rath3rApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the rath3rApp
+ */
+angular.module('rath3rApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
