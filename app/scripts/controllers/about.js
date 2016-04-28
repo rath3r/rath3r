82d847fca31611a2c41f547b0ada67de0ecf9b80
Thomas Meehan <thomas.meehan@living-group.com>
2016-04-28 21:46:30 +0100

    v6

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -15,10 +15,11 @@ rath3rApp.controller('aboutCtrl', function($scope, $http) {
 
     $scope.loading = true;
 
-    $http.get('http://dev.rath3rapi.com/json').
+    $http.get('http://data.rath3r.com/json').
         success(function(data) {
 
-            console.log(data);
+            loadData(data);
+
             $scope.loading = false;
 
         }).
@@ -28,4 +29,102 @@ rath3rApp.controller('aboutCtrl', function($scope, $http) {
         // or server returns response with an error status.
         });
 
+    function loadData(skills) {
+
+        var skillsdatelast = Math.max.apply(null,
+                Object.keys(skills).map(function(e) {
+                    return Date.parse(skills[e].dateStarted);
+                })
+            ),
+            skillsdatefirst = Math.min.apply(null,
+                Object.keys(skills).map(function(e) {
+                    return Date.parse(skills[e].dateStarted);
+                })
+            ),
+            currentDateObj = new Date(),
+            currentDate = currentDateObj.getTime(),
+            currentYear,
+            data = [],
+            diff,
+            translateY,
+            currentDiff,
+            width = 700,
+            barHeight = 20,
+            chart,
+            bar,
+            myDate,
+            currentWidth,
+            translateWidthY,
+            skillsdatefirstObj,
+            skillStartYear;
+
+        diff = currentDate - skillsdatefirst;
+
+        skillsdatefirstObj = new Date(skillsdatefirst);
+        skillStartYear = skillsdatefirstObj.getFullYear();
+        currentYear = currentDateObj.getFullYear();
+
+        for(var i = 0; i < skills.length; i++){
+
+            currentDiff = currentDate - Date.parse(skills[i].dateStarted);
+
+            if (diff == currentDiff) {
+                translateY = 0;
+            } else {
+                translateY = (currentDiff * width) / diff;
+            }
+
+
+            if (skills[i].stillUsing == 1) {
+                translateWidthY = (currentDate * width) / diff;
+            } else {
+                currentWidth = Date.parse(skills[i].dateFinished);
+                translateWidthY = ((currentDate - currentWidth) * width) / diff;
+            }
+
+            data.push(
+                {
+                    y       : translateY,
+                    title   : skills[i].title,
+                    width   : translateWidthY
+                }
+            );
+        }
+
+        chart = d3.select(".chart")
+            .attr("width", width)
+            .attr("height", ((barHeight * data.length) + 100));
+
+        bar = chart.selectAll("g")
+            .data(data)
+            .enter().append("g")
+            .attr("transform", function(d, i) { return "translate(" + d.y + "," + ((i * barHeight) + 20) + ")"; });
+
+        bar.append("rect")
+            .attr("width", function(d, i) { return d.width; })
+            .attr("height", barHeight - 1);
+
+        bar.append("text")
+            .attr("x", function(d) { return 10; })
+            .attr("y", barHeight / 2)
+            .attr("dy", ".35em")
+            .text(function(d) { return d.title; });
+
+        d3.select(".chart")
+            .append("text")
+            .attr("class", "label")
+            .attr("x", 0)
+            .attr("y", 10)
+            .attr("dy", ".35em")
+            .text(skillStartYear);
+
+        d3.select(".chart")
+            .append("text")
+            .attr("class", "label")
+            .attr("x", (width - 25))
+            .attr("y", 10)
+            .attr("dy", ".35em")
+            .text(currentYear);
+
+    }
 });
