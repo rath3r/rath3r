19b5bf1ef3f1e1187332d8f755d712f218d68c68
Thomas Meehan <thom.meehan@gmail.com>
2021-12-16 21:43:45 +0000

    ts server

---
+++ server/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+
+// import React from "react";
+// import ReactDOMServer from "react-dom/server";
+
+// import App from "../src/App";
+
+const PORT = 8000;
+
+const app = express();
+
+app.use("^/$", (req, res, next) => {
+  fs.readFile(path.resolve("./public/index.html"), "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Some error happened");
+    }
+    return res.send(
+      data.replace(
+        '<div id="root"></div>',
+        `<div id="root"></div>`
+      )
+    );
+  });
+});
+
+app.use(express.static(path.resolve(__dirname, '..', 'public')))
+
+app.listen(PORT, () => {
+  console.log(`App launched on ${PORT}`);
+});
