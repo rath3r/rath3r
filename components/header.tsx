313bcd722b0e639fbfb4c9e01890298b3e39f716
Thomas Meehan <thom.meehan@gmail.com>
2022-01-06 23:57:36 +0000

    fist component

---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.css'
+const Header = () => {
+    return (
+        <header className={styles.title}>
+            <h1>Rath3r</h1>
+        </header>
+    )
+}
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Header from '../components/header'
 
 const Home: NextPage = () => {
   return (
@@ -13,6 +14,8 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+        <Header></Header>
+
         <h1 className={styles.title}>
           Rath3r
         </h1>
